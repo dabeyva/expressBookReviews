1bb71b378ee1aa70a06558641b6fa8c18484885c
dabeyva <dabeyva@gmail.com>
2026-08-11 19:41:28 -0400

    Completada Tarea 7: Inicio de sesion y JWT

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,24 +5,52 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+// Comprueba si el usuario existe en el arreglo
+const isValid = (username) => {
+  let userswithsameName = users.filter((user) => {
+    return user.username === username;
+  });
+  return userswithsameName.length > 0;
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+// Comprueba si el usuario y la contraseña coinciden
+const authenticatedUser = (username, password) => {
+  let validusers = users.filter((user) => {
+    return (user.username === username && user.password === password);
+  });
+  return validusers.length > 0;
 }
 
-//only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// Tarea 7: Iniciar sesión como usuario registrado
+regd_users.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    return res.status(404).json({ message: "Error al cargar los datos de inicio de sesión" });
+  }
+
+  if (authenticatedUser(username, password)) {
+    // Generar Token JWT
+    let accessToken = jwt.sign({
+      data: password
+    }, 'access', { expiresIn: 60 * 60 });
+
+    // Guardar token y usuario en la sesión
+    req.session.authorization = {
+      accessToken,
+      username
+    };
+
+    return res.status(200).send("Usuario autenticado con éxito");
+  } else {
+    return res.status(208).json({ message: "Credenciales de inicio de sesión no válidas" });
+  }
 });
 
-// Add a book review
+// Add a book review (Tarea 8 - la haremos a continuación)
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 module.exports.authenticated = regd_users;
