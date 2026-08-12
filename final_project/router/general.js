0841a7f6d3f85fdbd911e2cdaa03650713c1fe15
dabeyva <dabeyva@gmail.com>
2026-08-11 20:14:56 -0400

    Completada Tarea 11: Buscar libro por ISBN usando Promesas

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -42,15 +42,28 @@ public_users.get('/', function (req, res) {
       });
   });
 
-// Get book details based on ISBN
+
+// Get book details based on ISBN using Promises (Tarea 11)
 public_users.get('/isbn/:isbn', function (req, res) {
     const isbn = req.params.isbn;
-    if (books[isbn]) {
-      return res.status(200).send(JSON.stringify(books[isbn], null, 4));
-    } else {
-      return res.status(404).json({ message: "Libro no encontrado" });
-    }
+  
+    const getBookByISBN = new Promise((resolve, reject) => {
+      if (books[isbn]) {
+        resolve(books[isbn]);
+      } else {
+        reject("Libro no encontrado");
+      }
+    });
+  
+    getBookByISBN
+      .then((book) => {
+        return res.status(200).send(JSON.stringify(book, null, 4));
+      })
+      .catch((error) => {
+        return res.status(404).json({ message: error });
+      });
   });
+
   
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
